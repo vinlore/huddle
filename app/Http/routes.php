dea0e09bcd441093648734c07d8131006c2ed6a9
Vincent Lore <vincent_lore@hotmail.com>
2016-02-11 18:49:14 -0800

    Fix routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,10 +9,32 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
+/**
+ * HOME PAGE
+ */
 Route::get('/', function () {
-    return view('welcome');
+	return View::make('index');
+});
+
+// Laravel welcome page for demonstration
+Route::get('welcome', function () {
+	return View::make('welcome');
+});
+
+/**
+ * API ROUTES
+ */
+Route::group(['prefix' => 'api'], function () {
+	// example from one page comment app
+
+	// since we will be using this just for CRUD, we won't need create and edit
+	// Angular will handle both of those forms
+	// this ensures that a user can't access api/create or api/edit when there's nothing there
+	Route::resource('comments', 'CommentController', // URL at /api/comments
+		['only' => array('index', 'store', 'destroy')]);
+
 });
 
 /*
@@ -24,8 +46,8 @@ Route::get('/', function () {
 | it contains. The "web" middleware group is defined in your HTTP
 | kernel and includes session state, CSRF protection, and more.
 |
-*/
+ */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+	//
 });
