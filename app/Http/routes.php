f4c6c02ee9d32d39e8b44ac5dc1c8e01bc36f2ed
Christopher Yang <chris.ct.yang@gmail.com>
2016-03-02 16:22:32 -0800

    Format and set up Controller classes
    - Follow coding standards
    - Also correct routes accordingly
    - Temporarily comment out unused API routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,18 +24,18 @@ Route::get('welcome', function () {
  * Account - Login System
  */
  	//Authenticaiton and Login
-Route::post ('api/auth', 'Authenticate_Controller@user_authentication');
+Route::post ('api/auth', 'AuthenticateController@user_authentication');
 Route::get ('api/login', function(){
 	return View::make('login');
 });
 	//Registration page
-Route::post ('api/create', 'Authenticate_Controller@user_registration');
+Route::post ('api/create', 'AuthenticateController@user_registration');
 Route::get ('api/register', function() {
 	return View::make('register');
 });
 
 	//Activate the Account
-Route::get('api/activation', 'Authenticate_Controller@user_activation');
+Route::get('api/activation', 'AuthenticateController@user_activation');
 
 	//Log Out
 Route::post('api/logout', function(){
@@ -128,107 +128,106 @@ Route::get('email', function () {
 		  return "Protected resource";
 		});
 
-		/*
-		* 	Conference API
-		*/
-
-		//Return list of conferences
-		Route::get('conferences' , 'Conference_Controller@get_conferences');
-
-		//Returns a conference
-		Route::get('conferences' , 'Conference_Controller@get_conference');
-
-		//Delete a conference
-		Route::delete('conferences' , 'Conference_Controller@delete_conference');
-
-		//Creates a conference
-		Route::post('conferences' ,  'Conference_Controller@create_conference');
-
-		//Updates a conference
-		Route::put('conferences' , 'Conference_Controller@update_conference');
-
-		//Return list of attendees
-		Route::get('conferences/attendees' , 'Conference_Controller@get_attendees');
-
-		//Add an attendee
-		Route::post('conferences/attendees' , 'Conference_Controller@add_attendee');
-
-		//Delete an attendee
-		Route::delete('conferences/attendees' , 'Conference_Controller@delete_attendee');
-
-		//Update attendee
-		Route::put('conferences/attendees' , 'Conference_Controller@update_attendee');
-
-		//Returns list of accommodations
-		Route::get('conferences/accommodations' , 'Conference_Controller@get_accommodations');
-
-		//Adds accommodations
-		Route::post('conferences/accommodations' , 'Conference_Controller@add_accommodation');
-
-		//Delete accommodation
-		Route::delete('conferences/accommodations' , 'Conference_Controller@delete_accommodation');
-
-		//Update accommodation
-		Route::put('conferences/accommodations' , 'Conference_Controller@update_accommodation');
-
-		//Returns list of inventory items
-		Route::get('conferences/inventory' , 'Conference_Controller@get_inventory');
-
-		//Add item to inventory
-		Route::post('conferences/inventory' , 'Conference_Controller@add_item');
-
-		//Delete item from inventory
-		Route::delete('conferences/inventory' , 'Conference_Controller@delete_item');
-
-		//Update item from inventory
-		Route::put('conferences/inventory' , 'Conference_Controller@update_item');
-
-		/*
-		*	Event API
-		*/
-
-		//Returns a list of events
-		Route::get('conferences/events' , 'Event_Controller@get_events');
-
-		//Returns an events
-		Route::get('conferences/event' , 'Event_Controller@get_event');
-
-		//Deletes an event
-		Route::delete('conferences/events' , 'Event_Controller@delete_event');
-
-		//Create an events
-		Route::post('conferences/events' , 'Event_Controller@create_event');
-
-		//Update an event
-		Route::put('conferences/events' , 'Event_Controller@update_event');
-
-		//Return List of attendees
-		Route::get('conferences/events/attendees' , 'Event_Controller@get_attendees');
-
-		//Add attendee to event
-		Route::post('conferences/events/attendee' , 'Event_Controller@add_attendee');
-
-		//delete attendee from event
-		Route::delete('conferences/events/attendees' , 'Event_Controller@delete_attendee');
-
-		/*
-		*	Profile API
-		*/
-
-		//Returns a user Profile
-		Route::get('users/profile' , 'Profile_Controller@get_profile');
-
-		//Update profile
-		Route::put('users/profile' , 'Profile_Controller@update_profile');
-
-		//Update permissions
-		Route::post('permissions' , 'Profile_Controller@update_permissions');
-
-		//Delete users
-		Route::delete('users' , 'Profile_Controller@delete_user');
-
 		//Deactivate users
-		Route::put('users' , 'Profile_Controller@deactivate_user');
+		Route::put('users' , 'ProfileController@deactivateUser');
 
+		// /*
+		// * 	Conference API
+		// */
+
+		// //Return list of conferences
+		// Route::get('conferences' , 'Conference_Controller@get_conferences');
+
+		// //Returns a conference
+		// Route::get('conferences' , 'Conference_Controller@get_conference');
+
+		// //Delete a conference
+		// Route::delete('conferences' , 'Conference_Controller@delete_conference');
+
+		// //Creates a conference
+		// Route::post('conferences' ,  'Conference_Controller@create_conference');
+
+		// //Updates a conference
+		// Route::put('conferences' , 'Conference_Controller@update_conference');
+
+		// //Return list of attendees
+		// Route::get('conferences/attendees' , 'Conference_Controller@get_attendees');
+
+		// //Add an attendee
+		// Route::post('conferences/attendees' , 'Conference_Controller@add_attendee');
+
+		// //Delete an attendee
+		// Route::delete('conferences/attendees' , 'Conference_Controller@delete_attendee');
+
+		// //Update attendee
+		// Route::put('conferences/attendees' , 'Conference_Controller@update_attendee');
+
+		// //Returns list of accommodations
+		// Route::get('conferences/accommodations' , 'Conference_Controller@get_accommodations');
+
+		// //Adds accommodations
+		// Route::post('conferences/accommodations' , 'Conference_Controller@add_accommodation');
+
+		// //Delete accommodation
+		// Route::delete('conferences/accommodations' , 'Conference_Controller@delete_accommodation');
+
+		// //Update accommodation
+		// Route::put('conferences/accommodations' , 'Conference_Controller@update_accommodation');
+
+		// //Returns list of inventory items
+		// Route::get('conferences/inventory' , 'Conference_Controller@get_inventory');
+
+		// //Add item to inventory
+		// Route::post('conferences/inventory' , 'Conference_Controller@add_item');
+
+		// //Delete item from inventory
+		// Route::delete('conferences/inventory' , 'Conference_Controller@delete_item');
+
+		// //Update item from inventory
+		// Route::put('conferences/inventory' , 'Conference_Controller@update_item');
+
+		// /*
+		// *	Event API
+		// */
+
+		// //Returns a list of events
+		// Route::get('conferences/events' , 'Event_Controller@get_events');
+
+		// //Returns an events
+		// Route::get('conferences/event' , 'Event_Controller@get_event');
+
+		// //Deletes an event
+		// Route::delete('conferences/events' , 'Event_Controller@delete_event');
+
+		// //Create an events
+		// Route::post('conferences/events' , 'Event_Controller@create_event');
+
+		// //Update an event
+		// Route::put('conferences/events' , 'Event_Controller@update_event');
+
+		// //Return List of attendees
+		// Route::get('conferences/events/attendees' , 'Event_Controller@get_attendees');
+
+		// //Add attendee to event
+		// Route::post('conferences/events/attendee' , 'Event_Controller@add_attendee');
+
+		// //delete attendee from event
+		// Route::delete('conferences/events/attendees' , 'Event_Controller@delete_attendee');
+
+		// /*
+		// *	Profile API
+		// */
+
+		// //Returns a user Profile
+		// Route::get('users/profile' , 'Profile_Controller@get_profile');
+
+		// //Update profile
+		// Route::put('users/profile' , 'Profile_Controller@update_profile');
+
+		// //Update permissions
+		// Route::post('permissions' , 'Profile_Controller@update_permissions');
+
+		// //Delete users
+		// Route::delete('users' , 'Profile_Controller@delete_user');
 
 	});
