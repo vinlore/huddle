fc8b67d0f8b397e386b30102d2218959208e352b
Haniel Martino <haniel_martino@hotmail.com>
2016-03-06 21:31:12 -0800

    Done profile and emergency Contact view for conf signup

--- public/components/profile/profileController.js
+++ public/components/profile/profileController.js
@@ -2,20 +2,20 @@ angular.module ( 'profileCtrl', [] )
 .controller('profileController', function ( $scope ) {
 
   $scope.user = {
-      "Username": null,
-      "OldPassword": null,
-      "NewPassword": null,
-      "ConfirmPassword": null,
-      "FirstName": null,
-      "MiddleName": null,
-      "LastName": null,
-      "Birthdate": null,
-      "Gender": null,
-      "Country": null,
-      "City": null,
-      "Email": null,
-      "HomePhone": null,
-      "OtherPhone": null
+      Username: null,
+      OldPassword: null,
+      NewPassword: null,
+      ConfirmPassword: null,
+      FirstName: null,
+      MiddleName: null,
+      LastName: null,
+      Birthdate: null,
+      Gender: null,
+      Country: null,
+      City: null,
+      Email: null,
+      HomePhone: null,
+      OtherPhone: null
   };
 
   $scope.saveChanges = function () {
--- public/components/signupConference/signupConferenceController.js
+++ public/components/signupConference/signupConferenceController.js
@@ -2,21 +2,57 @@ angular.module('signupConfCtrl',[])
 .controller('signupConferenceController', function($scope){
 
   $scope.user = {
-      "Username": null,
-      "OldPassword": null,
-      "NewPassword": null,
-      "ConfirmPassword": null,
-      "FirstName": null,
-      "MiddleName": null,
-      "LastName": null,
-      "Birthdate": null,
-      "Gender": null,
-      "Country": null,
-      "City": null,
-      "Email": null,
-      "HomePhone": null,
-      "OtherPhone": null
+      Username: null,
+      OldPassword: null,
+      NewPassword: null,
+      ConfirmPassword: null,
+      FirstName: null,
+      MiddleName: null,
+      LastName: null,
+      Birthdate: null,
+      Gender: null,
+      Country: null,
+      City: null,
+      Email: null,
+      HomePhone: null,
+      OtherPhone: null,
+      FamilyMembers : [
+        {
+          FirstName: null,
+          MiddleName: null,
+          LastName: null,
+          Birthdate: null,
+          Gender: null,
+          Country: null,
+          City: null,
+        }
+      ]
   };
+
+  $scope.emergencyContact = {
+    FirstName: null,
+    MiddleName: null,
+    LastName: null,
+    PhoneNumber: null,
+    Email: null
+  }
+
+  $scope.conference = {
+      name: "Indian Conference",
+      country: null,
+      city: null,
+      address: null,
+      startDate: null,
+      endDate: null,
+      description: null
+  }
+
+  $scope.addFamilyMember = function () {
+    var member = {
+
+    }
+    user.FamilyMembers.push(member);
+  }
 
   $scope.submitRequest = function () {
 
