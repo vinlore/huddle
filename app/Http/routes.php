cf59268c04d5a89161a38401c104b7a585b478b2
Christopher Yang <chris.ct.yang@gmail.com>
2016-03-08 05:53:20 -0800

    Refactor routes.php
    - Remove routes for unused Blade templates
    - Group ‘api’ routes together
    - Separate routes for testing
    - Rewrite testing routes for easier testing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,233 +1,70 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
- */
+// -----------------------------------------------------------------------------
+// API
+// -----------------------------------------------------------------------------
 
-/**
- * HOME PAGE
- */
-
-// Laravel welcome page for demonstration
-Route::get('welcome', function () {
-	return View::make('welcome');
+// Prefix all API routes with 'api'.
+Route::group(['prefix' => 'api', 'middleware' => ['throttle:50,1',]], function () {
+    Route::post('create', 'AuthenticateController@user_registration');
+    Route::post('auth', 'AuthenticateController@user_authentication');
+    Route::post('logout', 'AuthenticateController@logout');
 });
 
-/**
- * Account - Login System
- */
- 	//Authenticaiton and Login
-Route::post ('api/auth', 'AuthenticateController@user_authentication');
-Route::get ('api/login', function(){
-	return View::make('login');
-});
-	//Registration page
-Route::post ('api/create', 'AuthenticateController@user_registration');
-Route::get ('api/register', function() {
-	return View::make('register');
-});
+// -----------------------------------------------------------------------------
+// TESTING
+// -----------------------------------------------------------------------------
 
-	//Activate the Account
-Route::get('api/activation', 'AuthenticateController@user_activation');
+// Test DDoS protection.
+// throttle{requests/minute}
+Route::get('api/ddos', ['middleware' => 'throttle:10,1', function () {
+    return 'Hello!';
+}]);
 
-	//Log Out
-Route::post('api/logout', 'AuthenticateController@logout');
-	//Password reset
-Route::get('api/passwordreset',function(){
-	$reminder_code = $_GET['reminder_code'];
-	$new_password = $_GET['new_pass'];
-	$user = Sentinel::findById(5);
-	//return Reminder::create($user);
-
-	if ($reminder = Reminder::complete($user, $reminder_code, $new_password)){
-		return "{'sucess' : true}";
-	}else{
-	    return "{'success' : true, 'error' : { 'code' : 'Apollo', 'message' : 'Problem occured trying to reset password'}}";
-	}
-
+// Create a role called 'Admin'.
+Route::get('createRole', function () {
+    $role = Sentinel::getRoleRepository()->createModel()->create([
+        'name' => 'Admin',
+        'slug' => 'Admin',
+    ]);
+    var_dump($role);
 });
 
-/*
- * DEV - TESTING PAGES
- */
- //DDOS protection ['middleware' => 'throttle{requests/ minute}']
- Route::get('api/ddos', ['middleware' => 'throttle:10,1', function() {
-     return "Hello!";
- }]);
-
-	//Creating roles - Called Admin
-Route::get('createRole', function(){
-	$role = Sentinel::getRoleRepository()->createModel()->create([
-    'name' => 'Admin',
-    'slug' => 'Admin',]);
-	var_dump($role);
+// Assign the 'Admin' role to the first user.
+Route::get('assignRole', function () {
+    $user = Sentinel::findById(1);
+    $role = Sentinel::findRoleByName('Admin');
+    $role->users()->attach($user);
+    var_dump($role);
 });
 
-Route::get('assignRole', function(){
-
-	$user = Sentinel::findById(50);
-	$role = Sentinel::findRoleByName('Admin');
-	$role->users()->attach($user);
-	var_dump($role);
+// Remove the 'Admin' role from the first user.
+Route::get('removeRole', function () {
+    $user = Sentinel::findById(1);
+    $role = Sentinel::findRoleByName('Admin');
+    $role->users()->detach($user);
+    var_dump($role);
 });
 
-Route::get('removeRole', function(){
-
-	$user = Sentinel::findById(50);
-	$role = Sentinel::findRoleByName('Admin');
-	$role->users()->detach($user);
-	var_dump($role);
+// Add permissions to the 'Admin' role.
+Route::get('addPermissions', function () {
+    $role = Sentinel::findRoleByName('Admin');
+    $role->permissions = [
+        'event.update' => true,
+        'event.view'   => false,
+    ];
+    $role->save();
+    var_dump($role);
 });
 
-Route::get('addRolePermission', function(){
-	$role = Sentinel::findRoleById(1);
-
-	$role->permissions = [
-    	'event.update' => true,
-    	'event.view' => false,
-	];
-
-	$role->save();
-	var_dump($role);
+// Reset the first user's password.
+Route::get('api/passwordreset',function () {
+    $reminderCode = $_GET['reminder_code'];
+    $newPassword = $_GET['new_pass'];
+    $user = Sentinel::first();
+    if ($reminder = Reminder::complete($user, $reminderCode, $newPassword)) {
+        return Response::json(array('success' => true,));
+    } else {
+        return Response::json(array('success' => false,));
+    }
 });
-
-Route::get('email', function () {
-	Mail::send('email',['name' => 'nada'] , function($message)
-	{
-
-    $message->to('gabrielahernandez@hotmail.ca')->subject('YEEZUS FOR PREZ');
-
-	});
-});
-
-/**
- * API ROUTES
- */
-
- /*
- |--------------------------------------------------------------------------
- | Application Routes
- |--------------------------------------------------------------------------
- |
- | This route group applies the "web" middleware group to every route
- | it contains. The "web" middleware group is defined in your HTTP
- | kernel and includes session state, CSRF protection, and more.
- |
-  */
-	//Security for all url with /api
-	Route::group(['middleware' => ['authToken','throttle:50,1'],'prefix' => 'api'], function () {
-		//Development Purposes
-		Route::get('tok' , function() {
-		  return "Protected resource";
-		});
-
-		//Deactivate users
-		Route::put('users' , 'ProfileController@deactivateUser');
-
-		// /*
-		// * 	Conference API
-		// */
-
-		// //Return list of conferences
-		// Route::get('conferences' , 'Conference_Controller@get_conferences');
-
-		// //Returns a conference
-		// Route::get('conferences' , 'Conference_Controller@get_conference');
-
-		// //Delete a conference
-		// Route::delete('conferences' , 'Conference_Controller@delete_conference');
-
-		// //Creates a conference
-		// Route::post('conferences' ,  'Conference_Controller@create_conference');
-
-		// //Updates a conference
-		// Route::put('conferences' , 'Conference_Controller@update_conference');
-
-		// //Return list of attendees
-		// Route::get('conferences/attendees' , 'Conference_Controller@get_attendees');
-
-		// //Add an attendee
-		// Route::post('conferences/attendees' , 'Conference_Controller@add_attendee');
-
-		// //Delete an attendee
-		// Route::delete('conferences/attendees' , 'Conference_Controller@delete_attendee');
-
-		// //Update attendee
-		// Route::put('conferences/attendees' , 'Conference_Controller@update_attendee');
-
-		// //Returns list of accommodations
-		// Route::get('conferences/accommodations' , 'Conference_Controller@get_accommodations');
-
-		// //Adds accommodations
-		// Route::post('conferences/accommodations' , 'Conference_Controller@add_accommodation');
-
-		// //Delete accommodation
-		// Route::delete('conferences/accommodations' , 'Conference_Controller@delete_accommodation');
-
-		// //Update accommodation
-		// Route::put('conferences/accommodations' , 'Conference_Controller@update_accommodation');
-
-		// //Returns list of inventory items
-		// Route::get('conferences/inventory' , 'Conference_Controller@get_inventory');
-
-		// //Add item to inventory
-		// Route::post('conferences/inventory' , 'Conference_Controller@add_item');
-
-		// //Delete item from inventory
-		// Route::delete('conferences/inventory' , 'Conference_Controller@delete_item');
-
-		// //Update item from inventory
-		// Route::put('conferences/inventory' , 'Conference_Controller@update_item');
-
-		// /*
-		// *	Event API
-		// */
-
-		// //Returns a list of events
-		// Route::get('conferences/events' , 'Event_Controller@get_events');
-
-		// //Returns an events
-		// Route::get('conferences/event' , 'Event_Controller@get_event');
-
-		// //Deletes an event
-		// Route::delete('conferences/events' , 'Event_Controller@delete_event');
-
-		// //Create an events
-		// Route::post('conferences/events' , 'Event_Controller@create_event');
-
-		// //Update an event
-		// Route::put('conferences/events' , 'Event_Controller@update_event');
-
-		// //Return List of attendees
-		// Route::get('conferences/events/attendees' , 'Event_Controller@get_attendees');
-
-		// //Add attendee to event
-		// Route::post('conferences/events/attendee' , 'Event_Controller@add_attendee');
-
-		// //delete attendee from event
-		// Route::delete('conferences/events/attendees' , 'Event_Controller@delete_attendee');
-
-		// /*
-		// *	Profile API
-		// */
-
-		// //Returns a user Profile
-		// Route::get('users/profile' , 'Profile_Controller@get_profile');
-
-		// //Update profile
-		// Route::put('users/profile' , 'Profile_Controller@update_profile');
-
-		// //Update permissions
-		// Route::post('permissions' , 'Profile_Controller@update_permissions');
-
-		// //Delete users
-		// Route::delete('users' , 'Profile_Controller@delete_user');
-
-	});
