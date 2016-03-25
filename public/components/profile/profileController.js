a9fff63580384e3f960c8a5b9eaa9ac9994f0605
Haniel Martino <haniel_martino@hotmail.com>
2016-03-25 15:24:12 -0700

    Implement API call to load attendees data to reports page

--- public/components/profile/profileController.js
+++ public/components/profile/profileController.js
@@ -115,4 +115,37 @@ angular.module( 'profileCtrl', [] )
 
     $scope.loadProfile();
 
+    $scope.conferences = [
+      {Time: "01:00:00",
+      Date: "Monday March 7, 2016",
+      Log: "James created a conference name India"},
+
+      {Time: "02:00:00",
+      Date: "Tuesday March 8, 2016",
+      Log: "Gabby is requesting to attend Canada conference"},
+
+      {Time: "03:00:00",
+      Date: "Wednesday March 9, 2016",
+      Log: "Viggy editted France conference"},
+
+      {Time: "04:00:00",
+      Date: "Wednesday March 9, 2016",
+      Log: "Chris approved Martin's conference attendance"},
+      {Time: "01:00:00",
+      Date: "Monday March 7, 2016",
+      Log: "James created a conference name India"},
+
+      {Time: "02:00:00",
+      Date: "Tuesday March 8, 2016",
+      Log: "Gabby is requesting to attend Canada conference"},
+
+      {Time: "03:00:00",
+      Date: "Wednesday March 9, 2016",
+      Log: "Viggy editted France conference"},
+
+      {Time: "04:00:00",
+      Date: "Wednesday March 9, 2016",
+      Log: "Chris approved Martin's conference attendance"},
+    ];
+
 } );
--- public/components/reports/reportsController.js
+++ public/components/reports/reportsController.js
@@ -1,82 +1,125 @@
 angular.module( 'reportsCtrl', [] )
 .controller( 'reportsController', function( $scope ) {
 
-    $scope.dataset1 = {
-        "chart": {
-            "caption": "Countries Represented by Attendees",
-            "subCaption": "2016",
-            "showBorder": "0",
-            "use3DLighting": "0",
-            "enableSmartLabels": "1",
-            "startingAngle": "30",
-            "showLabels": "1",
-            "showPercentValues": "1",
-            "showLegend": "1",
-            "defaultCenterLabel": "",
-            "centerLabel": "1",
-            "centerLabelBold": "1",
-            "showTooltip": "1",
-            "decimals": "0",
-            "useDataPlotColorForLabels": "1",
-            "theme": "fint",
-            "enableMultiSlicing": "1",
-            "radius3D": "4"
-        },
-        "data": [
-            {
-                "label": "Canada",
-                "value": "500"
-        },
-            {
-                "label": "India",
-                "value": "1500"
-        },
-            {
-                "label": "USA",
-                "value": "275"
-        },
-            {
-                "label": "Germany",
-                "value": "120"
-        }
+  $scope.dataset1 = {
+    "chart": {
+      "caption": "Countries Represented by Attendees",
+      "subCaption": "2016",
+      "showBorder": "0",
+      "use3DLighting": "0",
+      "enableSmartLabels": "1",
+      "startingAngle": "30",
+      "showLabels": "1",
+      "showPercentValues": "1",
+      "showLegend": "1",
+      "defaultCenterLabel": "",
+      "centerLabel": "1",
+      "centerLabelBold": "1",
+      "showTooltip": "1",
+      "decimals": "0",
+      "useDataPlotColorForLabels": "1",
+      "theme": "fint",
+      "enableMultiSlicing": "1",
+      "radius3D": "4"
+    },
+    "data": [
+      {
+        "label": "Canada",
+        "value": "500"
+      },
+      {
+        "label": "India",
+        "value": "1500"
+      },
+      {
+        "label": "USA",
+        "value": "275"
+      },
+      {
+        "label": "Germany",
+        "value": "120"
+      }
     ]
-    }
+  }
 
-    $scope.dataset2 = {
-        "chart": {
-            "caption": "Gender Representation",
-            "subCaption": "2016",
-            "showBorder": "0",
-            "use3DLighting": "0",
-            "enableSmartLabels": "1",
-            "startingAngle": "30",
-            "showLabels": "1",
-            "showPercentValues": "1",
-            "showLegend": "1",
-            "defaultCenterLabel": "",
-            "centerLabel": "1",
-            "centerLabelBold": "1",
-            "showTooltip": "1",
-            "decimals": "0",
-            "useDataPlotColorForLabels": "1",
-            "theme": "fint",
-            "enableMultiSlicing": "1",
-            "radius3D": "5"
-        },
-        "data": [
-            {
-                "label": "Males",
-                "value": "820"
-            },
-            {
-                "label": "Females",
-                "value": "1300"
-            },
-            {
-                "label": "Other",
-                "value": "275"
-            },
-        ]
-    }
+  $scope.dataset2 = {
+    "chart": {
+      "caption": "Gender Representation",
+      "subCaption": "2016",
+      "showBorder": "0",
+      "use3DLighting": "0",
+      "enableSmartLabels": "1",
+      "startingAngle": "30",
+      "showLabels": "1",
+      "showPercentValues": "1",
+      "showLegend": "1",
+      "defaultCenterLabel": "",
+      "centerLabel": "1",
+      "centerLabelBold": "1",
+      "showTooltip": "1",
+      "decimals": "0",
+      "useDataPlotColorForLabels": "1",
+      "theme": "fint",
+      "enableMultiSlicing": "1",
+      "radius3D": "5"
+    },
+    "data": [
+      {
+        "label": "Males",
+        "value": "820"
+      },
+      {
+        "label": "Females",
+        "value": "1300"
+      },
+      {
+        "label": "Other",
+        "value": "275"
+      },
+    ]
+  }
+
+  // $scope.attendees = []
+  //
+  // $scope.loadAttendees = function () {
+  //     Conferences.attendees().get( {cid: $stateParams.conferenceId} )
+  //         .$promise.then( function( response ) {
+  //             if ( response.status == 'success' && response.attendees ) {
+  //                 $scope.attendees = response.attendees;
+  //             } else {
+  //                 popup.error( 'Error', response.message );
+  //             }
+  //         }, function () {
+  //             popup.connection();
+  //         })
+  // }
+  //
+  // $scope.loadAttendees();
+  //
+  //
+  // $scope.femaleCount = 0;
+  // $scope.maleCount = 0;
+  // $scope.otherCount = 0;
+  //
+  // $scope.loadGenderDistribution = function() {
+  //     for (var i = 0; i < attendees.length; i++){
+  //       console.log(attendees[i].gender);
+  //       if (attendees.[i].gender == "Male"){
+  //           $scope.maleCount++;
+  //       } else if (attendees.[i].gender == "Female"){
+  //           $scope.femaleCount++;
+  //       } else {
+  //           $scope.otherCount++;
+  //       }
+  //     }
+  // }
+  //
+  // $scope.updateMyChartData = function() {
+  //   $scope.dataset2.data[0].value = ""+$scope.maleCount;
+  //   $scope.dataset2.data[1].value = ""+$scope.femaleCount;
+  //   $scope.dataset2.data[2].value = ""+$scope.otherCount;
+  // }
+  //
+  // $scope.updateMyChartData();
 
 })
