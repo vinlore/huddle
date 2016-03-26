d2fd91cfb88570212d8d4250d8f05bc94c96b30f
j-ma <jma2572@gmail.com>
2016-03-25 19:16:14 -0700

    [front-end][accommodations] Implement add/delete/save/cancel for accommodations and moved Rooms button to the end of the table

--- public/components/manageAccommodations/manageAccommodationsController.js
+++ public/components/manageAccommodations/manageAccommodationsController.js
@@ -1,9 +1,9 @@
 angular.module('manageAccommodationsCtrl',[])
 .controller('manageAccommodationsController', function($scope, ngTableParams, $stateParams, $filter, $location, $log){
 
-	var id = $stateParams.conferenceId;
+	$scope.conferenceId = $stateParams.conferenceId;
 
-	$log.log(id);
+	//////// Data Structures ////////
 
 	$scope.accommodations = [
 	{
@@ -22,19 +22,72 @@ angular.module('manageAccommodationsCtrl',[])
 	}
 	]
 
+	$scope.temp = []
+
+	// initial input data
+	$scope.accom = {
+    	name: null,
+    	address: null,
+    	city: null,
+    	country: null
+    }
+
+    //////// Intial State ////////
+
+	// copy actual data into a temp array for protection
+	$scope.temp = $scope.accommodations.slice();
+	$scope.hasChanges = false;
+
 	$scope.tableParams = new ngTableParams(
 	{
 	},
 	{
 		counts: [],
 		getData: function ($defer, params) {
-			$scope.data = params.sorting() ? $filter('orderBy') ($scope.accommodations, params.orderBy()) : $scope.accommodations;
+			$scope.data = params.sorting() ? $filter('orderBy') ($scope.temp, params.orderBy()) : $scope.temp;
 			$scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
 			$defer.resolve($scope.data);
 		}
 	});
 
+	//////// Button Functions ////////
+
 	$scope.open = function(id) {
 		$location.url('/manage-rooms-' + id);
 	}
+
+    $scope.add = function(accom) {
+    	$scope.hasChanges = true;
+
+    	// add new row to temp array
+    	$scope.temp.push(accom);
+
+    	// clear input data
+    	$scope.accom = null;
+
+    	$log.log($scope.temp);
+
+    	// refresh tableParams to reflect changes
+    	$scope.tableParams.reload();
+    }
+
+    $scope.del = function(index) {
+    	$scope.hasChanges = true;
+    	$scope.temp.splice(index, 1);
+    	$scope.tableParams.reload();
+    }
+
+    $scope.cancel = function() {
+    	$scope.hasChanges = false;
+
+  		// revert temp array to the same as original (i.e. row array)
+  		$scope.temp = $scope.accommodations.slice();
+  		$scope.tableParams.reload();
+  	}
+
+  	$scope.save = function() {
+  		$scope.hasChanges = false;
+  		$scope.accommodations = $scope.temp.slice();
+  	}
+
 })
