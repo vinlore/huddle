4edfa8842a4f748ed859dec6bde517bf02b195d0
Gabriela Hernandez <Gabriela Hernandez>
2016-03-28 09:20:09 -0700

    phpunit tests for conference and event [excluding gets]

--- tests/ConferenceTest.php
+++ tests/ConferenceTest.php
@@ -3,18 +3,135 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 
 class ConferenceTest extends TestCase{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-          // $this->json('POST', '/api/conferences', ['name' => 'CONFERENCE 1'])
-          //    ->seeJson([
-          //       'status'  => 'success'
-          //    ]);
+
+    use WithoutMiddleware;
+
+
+    public function testConferenceCreate(){
+        try{
+          Artisan::call('migrate:refresh');
+          Artisan::call('db:seed');
+
+          $response = $this->call('POST', '/api/auth/login', ['username' => 'admin', 'password' => 'password']);
+          $content = json_decode($response->getContent());
+
+          $conference = [
+            'name'        => 'Test Conference',
+            'description' => 'A conference in Canada.',
+            'start_date'  => '2016-05-11',
+            'end_date'    => '2016-05-20',
+            'address'     => '1055 Canada Pl, Vancouver, BC V6C 0C3, Canada',
+            'city'        => 'Vancouver',
+            'country'     => 'Canada',
+            'capacity'    => 1000,
+            'status'      => 'pending'
+          ];
+         $this->call('POST', '/api/conferences', $conference, [], [], ['HTTP_X-Auth-Token' => $content->token, 'HTTP_ID' => 1 ]);
+         $this->seeJson([
+                 'status'  => 200
+              ]) ;
+
+         }catch(Exception $e){
+          echo $e->getMessage();
+         }
+
     }
+
+    public function testConferenceUpdate(){
+      try{
+          $response = $this->call('POST', '/api/auth/login', ['username' => 'admin', 'password' => 'password']);
+          $content = json_decode($response->getContent());
+
+           $conference = [
+            'name'        => 'CHANGED',
+            'description' => 'CHANGED',
+            'start_date'  => '2016-05-11',
+            'end_date'    => '2016-05-20',
+            'address'     => 'CHANGED',
+            'city'        => 'CHANGED',
+            'country'     => 'CHANGED',
+            'capacity'    => 10
+          ];
+
+          $this->call('PUT', '/api/conferences/1', $conference, [], [], ['HTTP_X-Auth-Token' => $content->token, 'HTTP_ID' => 1 ]);
+          $this->seeJson([
+                 'status'  => 200
+              ]);
+        }catch(Exception $e){
+          echo '---------';
+          echo $e->getMessage();
+        }
+    }
+
+
+    public function testConferenceUpdateWithStatus(){
+
+      try{
+          $response = $this->call('POST', '/api/auth/login', ['username' => 'admin', 'password' => 'password']);
+          $content = json_decode($response->getContent());
+
+            $conference = [
+            'name'        => 'CHANGED',
+            'description' => 'CHANGED',
+            'start_date'  => '2016-05-11',
+            'end_date'    => '2016-05-20',
+            'address'     => 'CHANGED',
+            'city'        => 'CHANGED',
+            'country'     => 'CHANGED',
+            'capacity'    => 10,
+            'status'      =>'denied'
+          ];
+
+
+         $response= $this->call('PUT','/api/conferences/4', $conference, [], [], ['HTTP_X-Auth-Token' => $content->token, 'HTTP_ID' => 1 ]);
+          $this->seeJson([
+                 'status'  => 200
+              ]);
+        }catch(Exception $e){
+          echo '---------';
+          echo $e->getMessage();
+        }
+
+    }
+
+
+    public function testConferenceIndexWithStatus(){
+
+
+      try{
+          $response = $this->call('POST', '/api/auth/login', ['username' => 'admin', 'password' => 'password']);
+          $content = json_decode($response->getContent());
+
+          $response = $this->call('GET','/api/conferences/status/approved', ['status'=>'approved'], [], [], ['HTTP_X-Auth-Token' => $content->token, 'HTTP_ID' => 1 ]);
+          $this->seeJson([
+              'country' => 'Canada'
+             ]);
+        }catch(Exception $e){
+          echo '---------';
+          echo $e->getMessage();
+        }
+
+    }
+
+
+    public function testConferenceDestroy(){
+      try{
+          $response = $this->call('POST', '/api/auth/login', ['username' => 'admin', 'password' => 'password']);
+          $content = json_decode($response->getContent());
+
+          $this->call('DELETE','/api/conferences/4', [], [], [], ['HTTP_X-Auth-Token' => $content->token, 'HTTP_ID' => 1 ]);
+          $this->seeJson([
+                 'status'  => 200
+              ]);
+        }catch(Exception $e){
+          echo '---------';
+          echo $e->getMessage();
+        }
+
+    }
+
 }
--- tests/EventTest.php
+++ tests/EventTest.php
@@ -15,6 +15,8 @@ class EventTest extends TestCase{
      */
     public function testEventIndex()
     {
+          Artisan::call('migrate:refresh');
+          Artisan::call('db:seed');
         $this->json('GET','/api/conferences/4/events')
             ->seeJson([
                 'conference_id' => 4,
@@ -28,8 +30,11 @@ class EventTest extends TestCase{
     }
 
     /*
+
+    /*
     * Finding all events that are pending
     */
+    /*
     public function testIndexWithStatusPending()
     {
         $this->json('GET','/api/events-status',
@@ -43,11 +48,12 @@ class EventTest extends TestCase{
                 'name' => 'Project 3 Final Demos',
                 'description' => 'Teams 9 to 12 present their projects.',
             ]);
-    }
+    }*/
 
     /*
     * Finding all events that are approved
     */
+    /*
     public function testIndexWithStatusApproved()
     {
         $this->json('GET','/api/events-status',
@@ -61,6 +67,135 @@ class EventTest extends TestCase{
                 'address' => '17 Boulevard Saint-Jacques, Paris 75014, France',
                 'status' => 'approved',
             ]);
+    }   */
+
+     public function testEventCreate(){
+        try{
+          $response = $this->call('POST', '/api/auth/login', ['username' => 'admin', 'password' => 'password']);
+          $content = json_decode($response->getContent());
+
+           $event = [
+            'name'         => 'Test Event',
+            'conference_id' => 4,
+            'description'  => 'Welcome!',
+            'facilitator'  => 'TBD',
+            'date'         => '2016-05-01',
+            'start_time'   => '09:00',
+            'end_time'     => '10:00',
+            'address'      => 'Sansad Marg, Connaught Place, New Delhi, Delhi 110001, India',
+            'city'         => 'New Delhi',
+            'country'      => 'India',
+            'capacity'     => 1000,
+            'status'       => 'pending'
+        ];
+        $this->call('POST', '/api/conferences/4/events', $event, [], [], ['HTTP_X-Auth-Token' => $content->token, 'HTTP_ID' => 1 ]);
+        $this->seeJson([
+                 'status'  => 200
+              ]) ;
+         }catch(Exception $e){
+          echo $e->getMessage();
+         }
+
+    }
+
+
+    public function testEventUpdate(){
+      try{
+          $response = $this->call('POST', '/api/auth/login', ['username' => 'admin', 'password' => 'password']);
+          $content = json_decode($response->getContent());
+
+             $event = [
+            'name'         => 'CHANGED',
+            'conference_id' => 4,
+            'description'  => 'CHANGED',
+            'facilitator'  => 'CHANGED',
+            'date'         => '2016-05-01',
+            'start_time'   => '09:00',
+            'end_time'     => '10:00',
+            'address'      => 'CHANGED',
+            'city'         => 'CHANGED',
+            'country'      => 'CHANGED',
+            'capacity'     => 1000
+        ];
+
+          $this->call('PUT', '/api/conferences/1/events/4', $event, [], [], ['HTTP_X-Auth-Token' => $content->token, 'HTTP_ID' => 1 ]);
+          $this->seeJson([
+                 'status'  => 200
+              ]);
+        }catch(Exception $e){
+          echo '---------';
+          echo $e->getMessage();
+        }
+    }
+
+
+    public function testEventUpdateWithStatus(){
+
+      try{
+          $response = $this->call('POST', '/api/auth/login', ['username' => 'admin', 'password' => 'password']);
+          $content = json_decode($response->getContent());
+
+          $event = [
+            'name'         => 'CHANGED',
+            'conference_id' => 4,
+            'description'  => 'CHANGED',
+            'facilitator'  => 'CHANGED',
+            'date'         => '2016-05-01',
+            'start_time'   => '09:00',
+            'end_time'     => '10:00',
+            'address'      => 'CHANGED',
+            'city'         => 'CHANGED',
+            'country'      => 'CHANGED',
+            'capacity'     => 1000,
+            'status'       => 'denied'
+        ];
+
+
+         $this->call('PUT','/api/conferences/4/events/4', $event, [], [], ['HTTP_X-Auth-Token' => $content->token, 'HTTP_ID' => 1 ]);
+         $this->seeJson([
+                 'status'  => 200
+              ]);
+        }catch(Exception $e){
+          echo '---------';
+          echo $e->getMessage();
+        }
+
+    }
+
+
+    public function testEvetIndexWithStatus(){
+
+
+      try{
+          $response = $this->call('POST', '/api/auth/login', ['username' => 'admin', 'password' => 'password']);
+          $content = json_decode($response->getContent());
+
+          $this->call('GET','/api/events/pending', ['status'=>'approved'], [], [], ['HTTP_X-Auth-Token' => $content->token, 'HTTP_ID' => 1 ]);
+          $this->seeJson([
+              'country' => 'Canada'
+             ]);
+        }catch(Exception $e){
+          echo '---------';
+          echo $e->getMessage();
+        }
+
+    }
+
+
+    public function testEventDestroy(){
+      try{
+          $response = $this->call('POST', '/api/auth/login', ['username' => 'admin', 'password' => 'password']);
+          $content = json_decode($response->getContent());
+
+          $this->call('DELETE','/api/conferences/4/events/4', [], [], [], ['HTTP_X-Auth-Token' => $content->token, 'HTTP_ID' => 1 ]);
+          $this->seeJson([
+                 'status'  => 200
+              ]);
+        }catch(Exception $e){
+          echo '---------';
+          echo $e->getMessage();
+        }
+
     }
 
 }
