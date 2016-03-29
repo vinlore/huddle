6639714ebd16cdfda6eaee4e70e98100ec6d3109
Christopher Yang <chris.ct.yang@gmail.com>
2016-03-29 14:18:15 -0700

    Add a list of pre-made response messages

--- app/Providers/ResponseServiceProvider.php
+++ app/Providers/ResponseServiceProvider.php
@@ -15,14 +15,38 @@ class ResponseServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Response::macro('success', function ($status = 200, $message = 'OK') {
+        Response::macro('success', function ($status = 200, $message = NULL) {
+            switch ($status) {
+                case 200:
+                    $message = ($message == NULL) ? 'OK' : $message;
+                    break;
+                case 201:
+                    $message = ($message == NULL) ? 'Created' : $message;
+                    break;
+                case 204:
+                    $message = ($message == NULL) ? 'No Content' : $message;
+                    break;
+            }
+
             return Response::json([
                 'status'  => $status,
                 'message' => $message,
             ]);
         });
 
-        Response::macro('error', function ($status = 500, $message = 'Internal Server Error') {
+        Response::macro('error', function ($status = 500, $message = NULL) {
+            switch ($status) {
+                case 404:
+                    $message = ($message == NULL) ? 'Not Found' : $message;
+                    break;
+                case 409:
+                    $message = ($message == NULL) ? 'Conflict' : $message;
+                    break;
+                case 500:
+                    $message = ($message == NULL) ? 'Internal Server Error' : $message;
+                    break;
+            }
+
             return Response::json([
                 'status'  => $status,
                 'message' => $message,
