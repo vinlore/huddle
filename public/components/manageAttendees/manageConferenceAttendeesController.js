df3005682941f6769da7a493c708f8dd5adf0288
j-ma <jma2572@gmail.com>
2016-03-28 21:18:58 -0700

    [profile-attends] Able to approve and deny attendees for conferences

--- public/components/manageAttendees/manageConferenceAttendeesController.js
+++ public/components/manageAttendees/manageConferenceAttendeesController.js
@@ -1,5 +1,5 @@
 angular.module( 'manageConferenceAttendeesCtrl', [] )
-.controller( 'manageConferenceAttendeesController', function ($scope, ngTableParams, $stateParams, $filter, Conferences) {
+.controller( 'manageConferenceAttendeesController', function ($scope, ngTableParams, $stateParams, $filter, Conferences, popup) {
 
   // Conference ID
   $scope.conferenceId = $stateParams.conferenceId;
@@ -28,5 +28,41 @@ angular.module( 'manageConferenceAttendeesCtrl', [] )
 
     }
   });
+
+  //////// Button Functions ////////
+
+  $scope.approve = function(id) {
+
+    Conferences.attendees().update( {cid: $scope.conferenceId, pid: id}, {status: 'approved'})
+    .$promise.then( function( response ) {
+      if ( response.status == 200 ) {
+        console.log( 'Changes saved to profile_attends_conferences (approve)' );
+        popup.alert( 'success', 'Approve success.' );
+      } else {
+        popup.error( 'Error', response.message );
+      }
+    }, function () {
+      popup.connection();
+    })
+
+    $scope.tableParams.reload();
+  }
+
+  $scope.deny = function(id) {
+
+    Conferences.attendees().update( {cid: $scope.conferenceId, pid: id}, {status: 'denied'})
+    .$promise.then( function( response ) {
+      if ( response.status == 200 ) {
+        console.log( 'Changes saved to profile_attends_conferences (deny)' );
+        popup.alert( 'success', 'Deny success.' );
+      } else {
+        popup.error( 'Error', response.message );
+      }
+    }, function () {
+      popup.connection();
+    })
+
+    $scope.tableParams.reload();
+  }
 
 });
