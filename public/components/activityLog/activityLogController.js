abf3f300a4641d392932d9c362cc37a084e3c851
Haniel Martino <haniel_martino@hotmail.com>
2016-03-30 14:44:15 -0700

    Fix api call for activities

--- public/components/activityLog/activityLogController.js
+++ public/components/activityLog/activityLogController.js
@@ -2,47 +2,14 @@ angular.module ( 'activityCtrl', [] )
 .controller('activityLogController', function ( $scope, Activity, popup ) {
 
 
-  $scope.logs = [
-    {Time: "01:00:00",
-    Date: "Monday March 7, 2016",
-    Log: "James created a conference name India"},
-
-    {Time: "02:00:00",
-    Date: "Tuesday March 8, 2016",
-    Log: "Gabby is requesting to attend Canada conference"},
-
-    {Time: "03:00:00",
-    Date: "Wednesday March 9, 2016",
-    Log: "Viggy editted France conference"},
-
-    {Time: "04:00:00",
-    Date: "Wednesday March 9, 2016",
-    Log: "Chris approved Martin's conference attendance"},
-    {Time: "01:00:00",
-    Date: "Monday March 7, 2016",
-    Log: "James created a conference name India"},
-
-    {Time: "02:00:00",
-    Date: "Tuesday March 8, 2016",
-    Log: "Gabby is requesting to attend Canada conference"},
-
-    {Time: "03:00:00",
-    Date: "Wednesday March 9, 2016",
-    Log: "Viggy editted France conference"},
-
-    {Time: "04:00:00",
-    Date: "Wednesday March 9, 2016",
-    Log: "Chris approved Martin's conference attendance"},
-  ];
-
-  $scope.logz = []
+//
+  $scope.logs = []
 
   $scope.loadActivityLog = function () {
-      Activity.get()
+      Activity.query()
           .$promise.then( function( response ) {
               if (response) {
-                  $scope.logz = response;
-                  //console.log(logz);
+                  console.log(response);
               } else {
                   popup.error( 'Error', response.message );
               }
