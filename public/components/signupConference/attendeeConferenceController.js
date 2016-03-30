04bf12c604b790c3e4f12af13b651aa836bd0726
Haniel Martino <haniel_martino@hotmail.com>
2016-03-29 18:31:37 -0700

    Editing conference profile

--- public/components/signupConference/attendeeConferenceController.js
+++ public/components/signupConference/attendeeConferenceController.js
@@ -15,7 +15,37 @@ app.controller('attendeeConferenceController', function($scope, $stateParams, Co
       conference_id: $stateParams.conference_id,
       name: $stateParams.conference_name
   }
+  $scope.user = {}
 
+  $scope.loadAttendeeProfile = function() {
+    Conferences.attendees().get({cid: $stateParams.conference_id, pid: $stateParams.profile_id})
+    .$promise.then(function(response){
+      if(response){
+        var profile = response.pivot;
+        $scope.user = profile;
+        console.log(profile);
+      } else {
+        popup.error( 'Error', response.message );
+      }
+    }, function () {
+      popup.connection();
+    })
+  };
+  $scope.loadAttendeeProfile();
 
+  $scope.submitRequest = function(){
+      $scope.user.status = 'pending';
+      Conferences.attendees().update({cid: $stateParams.conference_id, pid: $stateParams.profile_id}, $scope.user)
+      .$promise.then( function ( response ) {
+        if ( response ) {
+            $state.go('profile');
+            popup.alert( 'success', "Conference profile successfully updated" );
+        } else {
+            popup.error( 'Error', response.message );
+        }
+    }, function () {
+        popup.connection();
+    })
+  };
 
 })
