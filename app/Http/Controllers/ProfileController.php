ccbe04f18deb6f8daf12b81cf71dcfe787e817f3
Martin <m4rtin.t@gmail.com>
2016-03-31 05:46:49 -0700

    Query Profile Conference/Event vehicles and rooms

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -76,6 +76,43 @@ class ProfileController extends Controller
         }
     }
 
+    public function test()
+    {
+        var_dump($lol);
+    }
+    public function profileConferenceRooms($pid) {
+        return \DB::table('profiles')
+        ->where('profiles.id', $pid)
+        ->join('profile_attends_conferences', 'profile_attends_conferences.profile_id', '=', 'profiles.id')
+        ->join('conferences','conferences.id','=','profile_attends_conferences.conference_id')
+        ->join('conference_accommodations','conferences.id','=','conference_accommodations.conference_id')
+        ->join('rooms', 'conference_accommodations.accommodation_id','=','rooms.accommodation_id')
+        ->join('profile_stays_in_rooms','profile_stays_in_rooms.room_id', '=', 'rooms.id')
+        ->get(['room_no', 'profiles.id', 'conferences.id']);
+    }
+
+    public function profileConferenceVehicles($pid) {
+        return \DB::table('profiles')
+        ->where('profiles.id', $pid)
+        ->join('profile_attends_conferences', 'profile_attends_conferences.profile_id', '=', 'profiles.id')
+        ->join('conferences','conferences.id','=','profile_attends_conferences.conference_id')
+        ->join('conference_vehicles','conferences.id','=','conference_vehicles.conference_id')
+        ->join('vehicles', 'conference_vehicles.vehicle_id','=','vehicles.id')
+        ->join('profile_rides_vehicles','profile_rides_vehicles.vehicle_id', '=', 'vehicles.id')
+        ->get(['vehicles.name','conferences.id']);
+    }
+
+    public function profileEventVehicles($pid) {
+        return \DB::table('profiles')
+        ->where('profiles.id', $pid)
+        ->join('profile_attends_events', 'profile_attends_events.profile_id', '=', 'profiles.id')
+        ->join('events','events.id','=','profile_attends_events.event_id')
+        ->join('event_vehicles','events.id','=','event_vehicles.event_id')
+        ->join('vehicles', 'event_vehicles.vehicle_id','=','vehicles.id')
+        ->join('profile_rides_vehicles','profile_rides_vehicles.vehicle_id', '=', 'vehicles.id')
+        ->get(['vehicles.name','events.id']);
+    }
+
     public function events(ProfileRequest $request, $id)
     {
         try {
