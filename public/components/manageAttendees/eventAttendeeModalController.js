507a7cc4e433702363ec4e45648cb1d465a68db9
j-ma <jma2572@gmail.com>
2016-03-31 06:37:26 -0700

    [front-end][attendees] Able to approve attendees by matching to vehicles

---
+++ public/components/manageAttendees/eventAttendeeModalController.js
@@ -0,0 +1,51 @@
+angular.module('eventAttendeeModalCtrl', [])
+.controller('eventAttendeeModalController', function ($scope, $uibModalInstance, Events, preferences, eventId) {
+    
+    $scope.preference = preferences;
+    $scope.user = {
+        arrivalVehicle: null,
+        departureVehicle: null,
+        room: null
+    }
+
+    $scope.arrivalVehicles, $scope.departVehicles = [];
+
+    $scope.loadArrivalVehicles = function () {
+        Events.vehicles().query( {eid: eventId, type: 'arrival'} )
+            .$promise.then( function( response ) {
+                if ( response ) {
+                    $scope.arrivalVehicles = response;
+                } else {
+                }
+            }, function () {
+                popup.connection();
+            })
+    }
+
+    $scope.loadArrivalVehicles();
+
+    $scope.loadDepartVehicles = function () {
+        Events.vehicles().query( {eid: eventId, type: 'departure'} )
+            .$promise.then( function( response ) {
+                if ( response ) {
+                    $scope.departVehicles = response;
+                } else {
+                }
+            }, function () {
+                popup.connection();
+            })
+    }
+
+    $scope.loadDepartVehicles();
+
+    $scope.approve = function () {
+        if ($scope.attendeeForm.$valid) {
+            $uibModalInstance.close($scope.user);
+        }
+    }
+
+    $scope.close = function () {
+        $uibModalInstance.close(false);
+    }
+
+})
