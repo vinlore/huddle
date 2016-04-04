a26570f4c027a9ef546ff085ca20a2bf973cc7fb
Christopher Yang <chris.ct.yang@gmail.com>
2016-04-03 23:01:45 -0700

    Format routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,7 +24,7 @@ Route::group(['prefix' => 'api', 'middleware' => ['throttle:50,1']], function ()
     // ROLES
     // -------------------------------------------------------------------------
 
-    Route::resource('roles' , 'RoleController', ['only' =>[
+    Route::resource('roles', 'RoleController', ['only' =>[
         'index', 'store', 'show', 'update', 'destroy',
     ]]);
 
@@ -41,7 +41,7 @@ Route::group(['prefix' => 'api', 'middleware' => ['throttle:50,1']], function ()
     // -------------------------------------------------------------------------
 
     Route::get('profile/{pid}/conferences', 'ProfileController@conferences');
-    Route::get('profile/{pid}/events','ProfileController@events');
+    Route::get('profile/{pid}/events', 'ProfileController@events');
     Route::get('profile/{pid}/rooms', 'ProfileController@rooms');
     Route::get('profile/{pid}/conferences/vehicles', 'ProfileController@conferenceVehicles');
     Route::get('profile/{pid}/events/vehicles', 'ProfileController@eventVehicles');
@@ -53,7 +53,7 @@ Route::group(['prefix' => 'api', 'middleware' => ['throttle:50,1']], function ()
     // CONFERENCES
     // -------------------------------------------------------------------------
 
-    Route::get('conferences/status/{status}' , 'ConferenceController@indexWithStatus');
+    Route::get('conferences/status/{status}', 'ConferenceController@indexWithStatus');
     Route::get('conferences/{cid}/events/status/{status}', 'ConferenceController@eventsWithStatus');
     Route::resource('conferences', 'ConferenceController', ['only' => [
         'index', 'store', 'show', 'update', 'destroy',
@@ -63,7 +63,7 @@ Route::group(['prefix' => 'api', 'middleware' => ['throttle:50,1']], function ()
     // EVENTS
     // -------------------------------------------------------------------------
 
-    Route::get('events/{status}' , 'EventController@indexWithStatus');
+    Route::get('events/{status}', 'EventController@indexWithStatus');
     Route::resource('conferences.events', 'EventController', ['only' => [
         'index', 'store', 'show', 'update', 'destroy',
     ]]);
@@ -116,23 +116,23 @@ Route::group(['prefix' => 'api', 'middleware' => ['throttle:50,1']], function ()
     // MANAGERS
     // -------------------------------------------------------------------------
 
-    Route::resource('conferences.managers' , 'ConferenceManagerController' , ['only' => [
-        'index' , 'store' , 'show' ,'update', 'destroy',
+    Route::resource('conferences.managers', 'ConferenceManagerController', ['only' => [
+        'index', 'store', 'show', 'update', 'destroy',
     ]]);
 
-    Route::resource('events.managers' , 'EventManagerController' , ['only' => [
-        'index' , 'store' , 'show' ,'update', 'destroy',
+    Route::resource('events.managers', 'EventManagerController', ['only' => [
+        'index', 'store', 'show', 'update', 'destroy',
     ]]);
 
     // -------------------------------------------------------------------------
     // ATTENDEES
     // -------------------------------------------------------------------------
 
-    Route::resource('conferences.attendees' , 'ConferenceAttendeeController' , ['only' => [
-        'index' , 'store' , 'show' ,'update', 'destroy',
+    Route::resource('conferences.attendees', 'ConferenceAttendeeController', ['only' => [
+        'index', 'store', 'show', 'update', 'destroy',
     ]]);
 
-    Route::resource('events.attendees' , 'EventAttendeeController' , ['only' => [
-        'index' , 'store' , 'show' ,'update', 'destroy',
+    Route::resource('events.attendees', 'EventAttendeeController', ['only' => [
+        'index', 'store', 'show', 'update', 'destroy',
     ]]);
 });
