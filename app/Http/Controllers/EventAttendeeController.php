4ef33d264063d4ac9e18a7331cb86ebd9b292349
Christopher Yang <chris.ct.yang@gmail.com>
2016-04-06 02:38:31 -0700

    Refactor EventAttendeeController.php

--- app/Http/Controllers/EventAttendeeController.php
+++ app/Http/Controllers/EventAttendeeController.php
@@ -10,27 +10,26 @@ use Sentinel;
 use App\Http\Requests\EventAttendeeRequest;
 
 use App\Models\Event;
+use App\Models\EventVehicle;
 use App\Models\Profile;
-use App\Models\Vehicle;
 
 class EventAttendeeController extends Controller
 {
     /**
      * Retrieve all Attendees of an Event.
      *
+     * @param  EventAttendeeRequest  $request
+     * @param  int  $eid
      * @return Collection|Response
      */
     public function index(EventAttendeeRequest $request, $eid)
     {
         try {
-
-            // Check if the Event exists.
             $event = Event::find($eid);
             if (!$event) {
                 return response()->error(404, 'Event Not Found');
             }
 
-            // Retrieve its Attendees.
             return $event->attendees()->get();
         } catch (Exception $e) {
             return response()->error();
@@ -40,28 +39,28 @@ class EventAttendeeController extends Controller
     /**
      * Create an Attendee for an Event.
      *
+     * @param  EventAttendeeRequest  $request
+     * @param  int  $eid
      * @return Response
      */
     public function store(EventAttendeeRequest $request, $eid)
     {
         try {
-
-            // Check if the Event exists.
             $event = Event::find($eid);
             if (!$event) {
                 return response()->error(404, 'Event Not Found');
             }
 
             $pid = $request->profile_id;
-
-            // Check if the Profile exists.
             $profile = Profile::find($pid);
             if (!$profile) {
                 return response()->error(404, 'Profile Not Found');
             }
 
             $profile->events()->attach($eid, $request->except('profile_id'));
-            $this->addActivity($request->header('ID'),'request', $eid, 'event application', $request->profile_id);
+
+            $this->addActivity($userId, $activityType, $eid, 'event application', $pid);
+
             return response()->success();
         } catch (Exception $e) {
             return response()->error($e);
@@ -71,25 +70,24 @@ class EventAttendeeController extends Controller
     /**
      * Retrieve an Attendee of an Event.
      *
+     * @param  EventAttendeeRequest  $request
+     * @param  int  $eid
+     * @param  int  $pid
      * @return App\Models\Profile|Response
      */
     public function show(EventAttendeeRequest $request, $eid, $pid)
     {
         try {
-
-            // Check if the Event exists.
             $event = Event::find($eid);
             if (!$event) {
                 return response()->error(404, 'Event Not Found');
             }
 
-            // Check if the Profile exists.
             $profile = Profile::find($pid);
             if (!$profile) {
                 return response()->error(404, 'Profile Not Found');
             }
 
-            // Retrieve the Attendee.
             return $event->attendees()->where('profile_id', $pid)->first();
         } catch (Exception $e) {
             return response()->error();
@@ -99,32 +97,33 @@ class EventAttendeeController extends Controller
     /**
      * Update an Attendee of an Event.
      *
+     * @param  EventAttendeeRequest  $request
+     * @param  int  $eid
+     * @param  int  $pid
      * @return Response
      */
     public function update(EventAttendeeRequest $request, $eid, $pid)
     {
         try {
+            $userId = $request->header('ID');
+            $profileOwnerId = $profile->user()->first()->id;
+            if ($userId != $profileOwnerId) {
+                if (!$this->isEventManager($request, $cid)) {
+                    return response()->error(403);
+                }
+            }
 
-            // Check if the Event exists.
             $event = Event::find($eid);
             if (!$event) {
                 return response()->error(404, 'Event Not Found');
             }
 
-            // Check if the Profile exists.
             $profile = Profile::find($pid);
             if (!$profile) {
                 return response()->error(404, 'Profile Not Found');
             }
 
-            $userId = $request->header('ID');
-            $profileOwnerId = $profile->user()->first()->id;
-
-            if ($userId != $profileOwnerId) {
-                if (!$this->isEventManager($request, $eid)) {
-                    return response()->error(403);
-                }
-            }
+            $activityType = 'updated';
 
             if ($request->exists('status')) {
 
@@ -134,28 +133,33 @@ class EventAttendeeController extends Controller
                 switch ($newStatus) {
                     case 'approved':
                         if ($oldStatus != 'pending') {
-                            return response()->error(403);
+                            break;
                         }
-                        $event->increment('attendee_count');
+                        $activityType = $newStatus;
+                        $conference->increment('attendee_count');
                         break;
                     case 'denied':
                         if ($oldStatus != 'pending') {
-                            return response()->error(403);
+                            break;
                         }
-                        // $event->decrement('attendee_count');
+                        $activityType = $newStatus;
                         break;
                     case 'pending':
                         if ($oldStatus != 'denied') {
-                            return response()->error(403);
+                            break;
                         }
+                        $activityType = 'resubmitted';
                         break;
                     default:
                         return response()->error(422);
                         break;
                 }
             }
+
             $profile->events()->updateExistingPivot($eid, $request->all());
+
             $this->addActivity($request->header('ID'),'update', $eid, 'event application', $pid);
+
             return response()->success();
         } catch (Exception $e) {
             return response()->error();
@@ -165,42 +169,34 @@ class EventAttendeeController extends Controller
     /**
      * Delete an Attendee of an Event.
      *
+     * @param  EventAttendeeRequest  $request
+     * @param  int  $eid
+     * @param  int  $pid
      * @return Response
      */
     public function destroy(EventAttendeeRequest $request, $eid, $pid)
     {
         try {
-            /*
-             * Detatch all related vehicles for this profile for this conference
-             */
-            //Find all the Vehicle_id associated with this profile
-            $vehicle_id = Profile::find($pid)->vehicles()->get(['id']);
-            //Loop through array of vehicle_id
-            foreach($vehicle_id as $vid)
-            {
-                //Grab all event_id associated to this vehicle
-                $eid = Vehicle::find($vid->id)
-                                ->events()
-                                ->get(['event_id']);
-               foreach($eid as $id)
-               {
-                   //if event_id matches the one being rejected
-                   if ($eid == $id->event_id)
-                   {
-                       Vehicle::find($vid->id)
-                               ->passengers()
-                               ->detach(Profile::find($pid));
-                   }
-               }
-
-               //Update Event Vehicle passenger
-               $passenger_count = Vehicle::find($vid->id)->passengers()->count();
-               Vehicle::where('id',$vid->id)->update(['passenger_count' => $passenger_count]);
+            $event = Event::find($cid);
+            if (!$event) {
+                return response()->error(404, 'Event Not Found');
             }
-            Profile::find($pid)->events()->detach($eid);
 
-            //Add Activity to log
-            $this->addActivity($request->header('ID'),'delete', $eid, 'event application', $pid);
+            $profile = Profile::find($pid);
+            if (!$profile) {
+                return response()->error(404, 'Profile Not Found');
+            }
+
+            $vehicles = $event->vehicles()->get();
+            foreach ($vehicles as $vehicle) {
+                if ($vehicle->passengers()->where('profile_id', $pid)->exists()) {
+                    $vehicle->passengers()->detach($profile);
+                    $vehicle->decrement('passenger_count');
+                }
+            }
+
+            $event->attendees()->detach($profile);
+            $event->decrement('attendee_count');
 
             return response()->success();
         } catch (Exception $e) {
