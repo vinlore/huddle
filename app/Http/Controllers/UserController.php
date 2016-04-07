ba01d0da36a163c91265d22f17aacf0508091bd4
Christopher Yang <chris.ct.yang@gmail.com>
2016-04-07 05:30:39 -0700

    Refactor UserController.php
    Pagination is ready to go, waiting for front-end to update accordingly
    so the manage accounts page doesn’t break.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,107 +5,73 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
+use Sentinel;
+
 use App\Http\Requests\UserRequest;
 
 use App\Models\User;
 
 class UserController extends Controller
 {
-
     /**
-     * Retrieve all Users.
+     * Retrieve all Users with usernames similar to a search string.
      *
+     * @param  UserRequest  $request
      * @return Collection|Response
      */
     public function index(UserRequest $request)
     {
-        // TODO pagination - Should only get certain rows
         try {
-            // Search users by username if $request->username is provided, then eliminate everything but letters and numbers
-            $username = preg_replace('/[^A-Za-z0-9]/', '', $request->username);
-            return User::where('username', 'like', '%'.$username.'%')->with('roles')->get(['id', 'username', 'email', 'permissions']);
+            $NON_ALPHA_NUM = '/[^A-Za-z0-9]/';
+            $query = preg_replace($NON_ALPHA_NUM, '', $request->username);
+            return User::where('username', 'like', '%'.$query.'%')
+                       ->with('roles')
+                       ->get();
+                       // ->paginate(10);
         } catch (Exception $e) {
-            return response()->error("500" , $e);
+            return response()->error();
         }
     }
 
     /**
      * Update a User.
      *
+     * @param  UserRequest  $request
+     * @param  int  $uid
      * @return Response
      */
-    public function update(UserRequest $request, $users)
+    public function update(UserRequest $request, $uid)
     {
-        /* Example JSON of request
-        *
-        $request =json_encode(array(
-             'api_token' => 1,
-             'user_id' =>  7,
-             'permissions' => array(
-                 'model.update' => true,
-                 'model.view' => true,
-             ),
-             'role_id' => 1,
-         ));
-         $request = json_decode($request);
-         */
-
         try {
-            //Check if Role Id exists
-            if(!\Sentinel::findRoleById($request->role_id))
-            {
-                return response()->error(404);
+            $user = Sentinel::findById($uid);
+            if (!$user) {
+                return response()->error(404, 'User Not Found');
             }
 
-            //Check if User Id Exists
-            if(!\Sentinel::findUserById($users))
-            {
-                return response()->error(404);
+            $rid = $request->role_id;
+            $role = Sentinel::findRoleById($rid);
+            if (!$role) {
+                return response()->error(404, 'Role Not Found');
             }
 
-            //Update Role first
-            $user = \Sentinel::findById($users);
-            $role = \Sentinel::findRoleById($request->role_id);
-
-            $user->roles()->sync([$role->id]);
-
-            //Update Permissions next
-            $user->permissions = json_decode(json_encode($request->permissions), True);
+            $user->roles()->sync([$rid]);
+            $user->permissions = $request->permissions;
             $user->save();
 
             return response()->success();
         } catch (Exception $e) {
-            return response()->error("500" , $e);
+            return response()->error();
         }
-    }
-
-    /**
-     * Delete a User.
-     *
-     * @return Response
-     */
-    public function destroy(UserRequest $request, $id)
-    {
-        //Not Allowed to destroy
     }
 
     /**
      * Reset the password of a User.
      *
+     * @param  UserRequest  $request
      * @return Response
      */
     public function resetPassword(UserRequest $request)
     {
-        try {
-            $user = Sentinel::findById($request->header('ID'));
-
-            if ($reminder = Reminder::complete($user, $request->code, $request->password))
-            {
-            // Reminder was successfull
-            return response()->success();
-            }
-        } catch (Exception $e) {
-            return response()->error("500" , $e);
-        }
+        // TODO?
     }
 }
