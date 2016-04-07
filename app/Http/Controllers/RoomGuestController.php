2a423e39e5b7c4619c7d39a07dbb5953e3ffcadc
Christopher Yang <chris.ct.yang@gmail.com>
2016-04-07 03:33:34 -0700

    Refactor RoomGuestController.php

--- app/Http/Controllers/RoomGuestController.php
+++ app/Http/Controllers/RoomGuestController.php
@@ -13,19 +13,18 @@ class RoomGuestController extends Controller
     /**
      * Retrieve all Guests of a Room.
      *
+     * @param  Request  $request
+     * @param  int  $rid
      * @return Collection|Response
      */
-    public function index($rid)
+    public function index(Request $request, $rid)
     {
         try {
-
-            // Check if the Room exists.
             $room = Room::find($rid);
             if (!$room) {
                 return response()->error(404, 'Room Not Found');
             }
 
-            // Retrieve its Guests.
             return $room->guests()->get();
         } catch (Exception $e) {
             return response()->error();
@@ -35,33 +34,29 @@ class RoomGuestController extends Controller
     /**
      * Create a Guest for a Room.
      *
+     * @param  Request  $request
+     * @param  int  $rid
      * @return Response
      */
     public function store(Request $request, $rid)
     {
         try {
-
-            // Check if the Room exists.
             $room = Room::find($rid);
             if (!$room) {
                 return response()->error(404, 'Room Not Found');
             }
 
-            // Check capacity.
-            if ($room->guest_count >= $room->capacity) {
-                return response()->error(422, 'Exceeded Capacity Error');
-            }
-
             $pid = $request->profile_id;
-
-            // Check if the Profile exists.
             $profile = Profile::find($pid);
             if (!$profile) {
-                return response()->error(404);
+                return response()->error(404, 'Profile Not Found');
             }
 
-            // Add the Guest.
-            $room->guests()->attach($pid);
+            if ($room->guest_count >= $room->capacity) {
+                return response()->error(422, 'Exceed Capacity Error');
+            }
+
+            $room->guests()->attach($profile);
             $room->increment('guest_count');
 
             return response()->success();
@@ -71,35 +66,26 @@ class RoomGuestController extends Controller
     }
 
     /**
-     * Retrieve a Guest of a Room.
-     *
-     * @return App\Models\Profile|Response
-     */
-    public function show($id){
-        //Show all the passengers within this one id.
-        try{
-            $room = Room::find($id)->guests()->get();
-            if(!$room){
-                return response()->success("204", "No Guests found.");
-            }
-            return $room;
-        } catch (Exception $e) {
-            return response()->error($e);
-        }
-    }
-
-    /**
      * Delete a Guest of a Room.
      *
+     * @param  Request  $request
+     * @param  int  $rid
+     * @param  int  $pid
      * @return Response
      */
-    public function destroy($rid, $pid) {
+    public function destroy(Request $request, $rid, $pid) {
         try {
-
-            Profile::find($pid)
-                    ->rooms()
-                    ->detach($room);
             $room = Room::find($rid);
+            if (!$room) {
+                return response()->error(404, 'Room Not Found');
+            }
+
+            $profile = Profile::find($pid);
+            if (!$profile) {
+                return response()->error(404, 'Profile Not Found');
+            }
+
+            $room->guests()->detach($profile);
             $room->decrement('guest_count');
 
             return response()->success();
